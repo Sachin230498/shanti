523b14149bd74b26235affe29236eafc22bcc9d6
Sachin230498 <sachinpathe230498@gmail.com>
2024-06-05 13:00:31 +0530

    add useEffect

---
+++ sejal/JS_3/closure/index.js
@@ -0,0 +1,13 @@
+function abc() {
+    let a = "123"
+    console.log(a);
+    
+    function abc2() {
+        let a = "456"
+        console.log(a);
+    }
+    abc2()
+
+}
+
+ abc()
---
+++ sejal/small_app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import UseEffect from './UseEffect';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React</h1>
+      <UseEffect/>
+    </div>
+  );
+}
+
+export default App;
