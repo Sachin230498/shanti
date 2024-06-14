d3ca515c06d4c964e80224747766ce10d41fda2d
Sachin230498 <sachinpathe230498@gmail.com>
2024-06-14 14:35:02 +0530

    test-2

---
+++ sejal/small_app/build/assets/js/coustom-carousel.js
@@ -0,0 +1,175 @@
+(function ($) {
+    'use strict';
+
+
+     //Portfolio list Home One
+    $('.portfolio-list-1').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        dots: true,
+        nav:false,
+        navText: ["<i class='bi bi-arrow-left''></i>", "<i class='bi bi-arrow-right''></i>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            600:{
+                items:1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            },
+            1920: {
+                items: 4
+            }
+        }
+    }) 
+
+    //Portfolio list Home Two
+    $('.portfolio-list-2').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        dots: true,
+        nav: true,
+        navText: ["<i class='bi bi-arrow-left''></i>", "<i class='bi bi-arrow-right''></i>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            600:{
+                items:1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            },
+            1920: {
+                items: 3
+            }
+        }
+    }) 
+
+        //Portfolio list Home Three
+    $('.portfolio-list-3').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        dots: false,
+        nav: false,
+        navText: ["<i class='bi bi-arrow-left''></i>", "<i class='bi bi-arrow-right''></i>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            600:{
+                items:1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            },
+            1920: {
+                items: 3
+            }
+        }
+    }) 
+
+     //tesit list One about inner
+    $('.testi-list-1').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        dots: true,
+        nav: true,
+        navText: ["<i class='bi bi-arrow-left''></i>", "<i class='bi bi-arrow-right''></i>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            600:{
+                items:1
+            },
+            768: {
+                items: 1
+            },
+            992: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            },
+            1920: {
+                items: 1
+            }
+        }
+    }) 
+
+     //Brand One About inner
+    $('.brand-list-1').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        dots: false,
+        nav: false,
+        navText: ["<i class='bi bi-arrow-left''></i>", "<i class='bi bi-arrow-right''></i>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            600:{
+                items:2
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 4
+            },
+            1000: {
+                items: 5
+            },
+            1920: {
+                items: 5
+            }
+        }
+    }) 
+
+
+
+
+
+})(jQuery);
+
--- sejal/small_app/src/App.js
+++ sejal/small_app/src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1>Deployment</h1>
+    </div>
   )
 }
 
