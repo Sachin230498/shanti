153b6726f7d4f1651b6388a65fb36dbd7a11a649
Tech-temple <sachinpathe123b@gmail.com>
2024-07-18 15:41:27 +0530

    add js

---
+++ Vedant/js-1/varibles/prob1.js
@@ -0,0 +1,64 @@
+// Varibles
+
+// Varibles are kinds of box where we can store data
+
+
+// let var const
+
+// Data type
+// String  ""
+// Number   
+// Boolean  
+// Array
+// Object
+// Undefined
+// Null
+
+
+// console.log 
+
+
+// let clothes = "Pants";
+// console.log(clothes);
+
+
+let container ;  //Declarations
+
+container = "Hello"  //Intialization 
+
+
+// console.log(container)
+
+container = "World" //Reassignment
+
+// console.log(container)
+
+//let container = "Abc" ; //Redeclare  =>SyntaxError: Identifier 'container' has already been declared
+// console.log(container)
+
+
+// In let varibles you can reassign but not redeclare the variables
+
+
+
+//var   you can reassign and redeclare the variables
+var a ; //declare
+ a = 12  //assign
+// console.log(a)
+
+
+a = 15  //reassign
+// console.log(a)
+
+var a = 16 //Redeclare 
+// console.log(a)
+
+
+// Const 
+
+const b = "abc" //declare and assign
+// console.log(b) Missing initializer in const declaration 
+
+b = 17 //Reassing
+console.log(b)  //TypeError: Assignment to constant variable.
+
---
+++ Vedant/js-1/varibles/prob2.js
@@ -0,0 +1,20 @@
+let num = 123;
+// let a = toString(num);
+// console.log(typeof a);
+// console.log(num)
+let str = "123";
+
+
+
+// let con =  Number(num);
+let con =  +(num);
+
+// console.log( typeof con);
+
+
+// console.log(typeof num)
+// console.log(typeof str)
+
+
+let b = false;
+console.log(b)
