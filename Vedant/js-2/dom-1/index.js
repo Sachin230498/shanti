6c13658366ac5c8b8d091230c5c4c3c7b41a9998
Tech-temple <sachinpathe123b@gmail.com>
2024-08-13 18:23:44 +0530

    add

---
+++ Vedant/js-2/dom-1/index.js
@@ -0,0 +1,61 @@
+// DOM = document object model
+
+// dom is a technique where you can access html element with the help of js
+
+
+console.log("hello")
+
+
+// function add(){
+//     console.log("namaste")
+// }
+
+// add()
+
+
+// console.log(document)
+
+// console.log(document.title)
+// console.log(document.domain)
+// console.log(document.URL)
+
+// console.log(document.links)
+// console.log(document.doctype)
+
+
+
+
+// Event => event is a action that is triggered by user
+
+
+
+
+
+// let h1 = document.getElementById("h1")
+// console.log(h1.innerText)
+
+// let btm =document.getElementById("btn")
+// console.log(btm.innerText)
+
+
+// let head =document.getElementById("head")
+// console.log(head.innerHTML)
+
+
+
+let count = document.getElementById("c")
+// console.log(count)
+
+let val = document.getElementById("v")
+let a = 0
+val.innerText = a
+
+function greet(){
+  count.innerText = "changed"
+  a++
+  val.innerText = a
+}
+
+
+
+
