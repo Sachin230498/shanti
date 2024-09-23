983e0a9d46b4739b28b6125a5d727a0c87a7f062
Sachin230498 <sachinpathe230498@gmail.com>
2024-09-23 17:17:32 +0530

    dom

---
+++ aditi/dom-3/TodoDOM/index.js
@@ -0,0 +1,98 @@
+let form = document.querySelector("form");
+let tbody = document.querySelector("tbody"); 
+
+form.addEventListener("submit", getData);
+let arr = [];
+
+function getData(){
+
+  event.preventDefault()
+  // console.log("hello")
+
+  let name = form.task.value;
+  let priority = form.priority.value;
+
+let obj = {
+  name,
+  priority:priority
+}
+
+arr.push(obj)
+display(arr)
+
+}
+
+
+
+function display(data){
+   tbody.innerHTML = null;
+
+    data.forEach(function(el){
+      // console.log(el)
+
+      let row = document.createElement("tr")
+
+      let task = document.createElement("td")
+      task.innerText = el.name;
+      let col2 = document.createElement("td")
+      col2.innerText = el.priority;
+      if(el.priority =="High"){
+        col2.style.backgroundColor = "red"
+      }else{
+        col2.style.backgroundColor = "green"
+      }
+      let col3 = document.createElement("td")
+      col3.style.backgroundColor = "red"
+      col3.innerText = "Delete"
+      
+      row.append(task,col2,col3);
+
+      tbody.append(row)
+
+
+    })
+}
+
+
+
+
+// let btn = document.querySelector("button")
+
+
+// btn.addEventListener("click", fun1)
+
+// function fun1(e){
+// console.log(e.target.parentNode)
+// }
+
+
+
+// let ar = [1,2,3,4,5,6]
+
+// let x = ar.forEach(function(el,index,array){
+//   //  console.log(el,index)
+//      let x = array[index]*2
+//      console.log(x)
+//   // return el*2
+// })
+
+// // console.log(x)
+
+// let y = ar.map(function(el,index,array){
+// // console.log(el)
+// return el*2
+// })
+
+// console.log(y)
+
+
+
+// function abc(){
+//   let nam = "Rahul"
+
+//   return nam;
+// }
+
+
+// let a = abc()
+// console.log(a)
