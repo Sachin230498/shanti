b9de96988caea0b7c87c060a73c65ced4a1b04fd
Sachin230498 <sachinpathe230498@gmail.com>
2024-09-24 15:17:11 +0530

    add js-1

---
+++ python-3/js-1/varibles_meth_op/prob1.js
@@ -0,0 +1,68 @@
+// Varible  => it is like a box or container that store the data information
+// let 
+//var 
+//const
+
+
+
+
+var nam //declaration
+
+nam = "Reet"  //Assign
+
+
+nam = "Hello"  //Reassign
+// console.log(nam)
+
+var nam = "Bye"  //Redeclare
+
+//  console.log(nam)  //printing
+
+
+
+
+
+let a = "abc";
+// console.log(a)
+
+a = "xyz"  //Reassign
+// console.log(a)
+
+// let a = "hey"; 
+// console.log(a)  //SyntaxError: Identifier 'a' has already been declared
+
+
+// const age ;  //declare
+
+// console.log(age)  // Missing initializer in const declaration
+
+const age = "123"
+// console.log(age)
+
+// age = "45"
+console.log(age)  // Assignment to constant variable.
+
+
+
+
+// data types
+
+
+// String;
+// Number;
+// Bigint;
+// Boolean;
+// Undefined;
+// Null;
+// Symbol;
+// Object;
+
+
+// string ""
+// number 12 3 5
+// Boolean // true false
+
+
+
+
+
---
+++ python-3/js-1/varibles_meth_op/prob2.js
@@ -0,0 +1,33 @@
+// Mathematical operators in JavaScript  + - / *   %    **
+
+
+let a = 10;
+let b = 20;
+
+// let c = a / b;
+// console.log("Output is", c)
+
+
+//  Modulo or reminder operator %  
+
+let c = a % b   
+// console.log(c)
+
+
+let x =9
+let y = 4
+
+// console.log(x%y)
+
+
+// Exponential operator  **
+
+
+let ab = 2;
+let d = 3;
+
+console.log(ab**d)  //2**3   2*2*2
+
+
+
+
