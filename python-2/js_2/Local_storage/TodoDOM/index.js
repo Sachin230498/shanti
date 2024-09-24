a659fa5e8df7424d40205101895870a19f862def
Sachin230498 <sachinpathe230498@gmail.com>
2024-09-24 11:34:50 +0530

    add todo

--- python-2/js_2/Local_storage/TodoDOM/index.js
+++ python-2/js_2/Local_storage/TodoDOM/index.js
@@ -36,7 +36,7 @@ function getData(){
 
 function display(data){
 tbody.innerHTML = "";
-    data.forEach(function(el){
+    data.forEach(function(el,index){
   // console.log(el)
   let row = document.createElement("tr")
   let col1 = document.createElement("td")
@@ -49,7 +49,11 @@ tbody.innerHTML = "";
     col2.style.backgroundColor="green"
   }
   let col3 = document.createElement("td")
+  col3.addEventListener("click", function(){
+    delfun(index)
+  })
   col3.innerText = "Delete"
+
   col3.style.backgroundColor = "red"
 
   row.append(col1,col2,col3);
@@ -60,8 +64,30 @@ tbody.innerHTML = "";
 }
 
 
+function delfun(i){
+ 
+  // console.log(i)
+  arr.splice(i,1)
+  localStorage.setItem("arr", JSON.stringify(arr));
+  display(arr)
+
+  // console.log(event.target.parentNode)
+  // event.target.parentNode.remove()
+
+}
+
+
+
+
 
 // let arr1 = [1,2,3,4,6]
+
+// // splice(index,  no of element you want to delete, no of element you want to add)
+
+// arr1.splice(1, 2,45,66,48,55,"abc")
+
+// console.log(arr1)
+
 
 // for(let i =0;i<arr1.length;i++){
 //   // console.log(arr1[i], i)
@@ -78,7 +104,9 @@ tbody.innerHTML = "";
 // localStorage.setItem("data", )
 
 
-let btn = document.querySelector("button")
-btn.addEventListener("click", getfun)
+// let btn = document.querySelector("button")
+// btn.addEventListener("click", getfun)
 
-function
+// function getfun(event){
+//   console.log(event.target.parentNode)
+// }
