dc2fe37ecdd032ed4c52e43af145847d764ab964
Sachin230498 <sachinpathe230498@gmail.com>
2024-10-08 16:15:12 +0530

    varible

---
+++ badal/js/varible/prob1.js
@@ -0,0 +1,53 @@
+// let var const
+
+
+// var a //declare
+
+// a = 5; //intilize
+
+// console.log(a)
+
+// a = 10;   //reassign
+
+// console.log(a)
+
+// var a = 15; //Redeclare
+
+// console.log(a)
+
+
+
+// Let
+
+let  a ;  //declare
+a = 5; //assign
+// console.log(a)
+
+a = 10; //reassign
+
+// console.log(a)
+
+// let a = 20;  //redelcare
+// console.log(a)  //Identifier 'a' has already been declared
+
+
+
+// Const
+
+// const x ; //declare
+// console.log(x)  //SyntaxError: Missing initializer in const declaration
+
+// const x = 55;
+// console.log(x)
+
+// x = 65;
+// console.log(x)  //TypeError: Assignment to constant variable.
+
+
+
+// string   ""
+// number 
+// Boolean true false
+// undefined
+// null
+
---
+++ badal/js/varible/prob2.js
@@ -0,0 +1,42 @@
+// Mathemitcal operator       +      -     /    *     %      **
+
+
+// let a = 10;
+// let c = 7
+// let b = 2;
+// let d = 3;
+
+// console.log("Output is" , a/b)
+
+// console.log(c%b)   // 7%2
+
+
+// console.log(a**d)   // 10*10*10
+
+
+// console.log(b**d)  // 2*2*2
+
+
+
+// Comaprision operator  < > <= >= ==   !=  ===  !==
+// always return boolean
+
+
+
+let a = 5;
+let b = 5;
+let c = 10;
+let d = 13;
+let e = "10";
+
+// let x = d>=c
+// console.log(x)
+
+
+let x =  e==c   // "10"==10 
+// == only check the value not the data type
+// console.log(x)
+
+let y = e===c
+// === check the value and also check the data type
+console.log(y)
