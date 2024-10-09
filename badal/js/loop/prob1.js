89f3055c5beed81329925e7e8834ff3b60a5b5b4
Sachin230498 <sachinpathe230498@gmail.com>
2024-10-09 15:45:53 +0530

    loop

---
+++ badal/js/loop/prob1.js
@@ -0,0 +1,64 @@
+// increment and decrement
+
+let a = 5;
+ a = a+2;
+
+//  console.log(a)
+
+ a = a -1
+//  console.log(a)
+
+ a = a * 2
+//  console.log(a)
+
+ a = a/3;
+//  console.log(a)
+
+
+//  a+=2
+//  a-=2
+//  a*=4
+//  a/=2
+//  a%=3
+//  a**=3
+//  console.log(a)
+
+// Post increment or post decrement
+
+let x = 6;
+// x++;  
+// console.log(x)
+
+x--;
+// console.log(x)
+
+
+// Pre increment or pre decrement
+
+let y = 10;
+++y;
+// console.log(y)
+
+--y;
+// console.log(y)
+
+
+
+
+
+// difference
+
+// let ab = 8;
+
+// let cd = ab++;  //post increment
+
+// console.log(ab);
+// console.log(cd);
+
+
+let ab = 8;
+
+let cd = ++ab; //pre increment
+
+console.log(ab);
+console.log(cd);
