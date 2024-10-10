c9d9e25be2ceb39bccaab92f5076780e4da15dd7
Sachin230498 <sachinpathe230498@gmail.com>
2024-10-10 11:56:13 +0530

    loop

--- python-2/js-3/json_server/frontend/index.js
+++ python-2/js-3/json_server/frontend/index.js
@@ -2,24 +2,20 @@ let api = "http://localhost:3000/Data";
 
 let main = document.getElementById("main");
 
+getData();
 
-getData()
+async function getData() {
+  let res = await fetch(api);
+  // console.log(res)
 
-async  function getData(){
-
-    let res = await fetch(api)
-    // console.log(res)
-
-    let data = await res.json()
-    // console.log(data)
-    display(data)
-
+  let data = await res.json();
+  // console.log(data)
+  display(data);
 }
-
 
 function display(arr) {
   arr.forEach(function (el, index) {
-      console.log(el)
+    console.log(el);
 
     let div = document.createElement("div");
 
@@ -28,69 +24,102 @@ function display(arr) {
     let price = document.createElement("h3");
     price.innerText = `Age :- ${el.age}`;
 
+   let update_div = document.createElement("div");    
+  update_div.setAttribute("id", "update_div")
+    let update = document.createElement("button");
+    update.innerText = "Update";
+   update.addEventListener("click", function(){
+    updatefun(el)
+   })
 
-    div.append(nam, price);
+   update_div.append(update);
+
+
+    let del = document.createElement("button");
+    del.addEventListener("click", function () {
+      deletefun(el.id);
+    });
+    del.innerText = "Delete";
+
+    div.append(nam, price, update_div, del);
 
     main.append(div);
   });
 }
 
+let form = document.querySelector("#post");
+// let update_form = document.getElementById("update");
 
 
-
-let form  = document.querySelector("#post")
-let update_form = document.getElementById("update");
-
-update_form.addEventListener("submit", updatefun)
-
-form.addEventListener("submit", PostData)
-
-async function PostData(){
-    event.preventDefault()
-
-    let name = form.name.value;
-    let age = form.age.value;
-
-    let data = {
-        name:name,
-        age:age
-    }
-
-    let res = await fetch(api, {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      }
-    });
-   
-}
+// update_form.addEventListener("submit", updatefun);
 
 
+form.addEventListener("submit", PostData);
 
-
-
-async function updatefun() {
+async function PostData() {
   event.preventDefault();
 
-  let name = update_form.uname.value;
-  let age = update_form.uage.value;
-  let id = update_form.id.value;
+  let name = form.name.value;
+  let age = form.age.value;
 
   let data = {
     name: name,
     age: age,
   };
 
-  console.log(data)
-
-  let res = await fetch(`${api}/${id}`, {
-    method: "PUT",
+  let res = await fetch(api, {
+    method: "POST",
     body: JSON.stringify(data),
     headers: {
       "Content-Type": "application/json",
     },
   });
-console.log("hello")
-
 }
+
+async function updatefun(data) {
+  event.preventDefault();
+console.log(data)
+  let update_div = document.getElementById("update_div");
+
+  update_div.innerHTML = ""
+
+  let name = document.createElement("input")
+  name.value = data.name
+  let age = document.createElement("input")
+  age.value = data.age
+  let update = document.createElement("button")
+  update.innerText = "save"
+
+  let obj = {
+    name:name,
+    age:age
+  }
+
+
+  update.addEventListener("click", async function(){
+      let res = await fetch(`${api}/${data.id}`, {
+        method: "PUT",
+        body: JSON.stringify({obj}),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+     
+    
+  })
+
+  update_div.append(name,age, update)
+}
+
+
+
+
+
+async function deletefun(id) {
+  event.preventDefault();
+  let res = await fetch(`${api}/${id}`, {
+    method: "DELETE",
+  });
+  console.log("hello");
+}
+
