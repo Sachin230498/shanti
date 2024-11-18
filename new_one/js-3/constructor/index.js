1f5838f185e87b9ff9bd1bfd36332827a53dbc23
Sachin230498 <sachinpathe230498@gmail.com>
2024-11-18 15:39:01 +0530

    advance js

---
+++ new_one/js-3/constructor/index.js
@@ -0,0 +1,44 @@
+// constructor 
+
+
+
+function nikeProd(){
+    obj = {};
+    obj.name = "Jorden"
+    obj.price = 12000
+
+    return obj;
+}
+
+
+// let a =  nikeProd()
+// console.log(a)
+
+
+
+// unsing constructor function
+
+function NikeCon(){
+    // this = {}
+      this.name = "run"
+      this.price =2000  
+}
+
+
+let con = new NikeCon()
+// console.log(con)
+
+
+// this = {}  object
+// new = return
+
+let string =new String()
+// console.log(string)
+
+let arr = new Array()
+// console.log(arr)
+
+
+let arr1 = [1,2,3,4]
+// console.log(arr1)
+
